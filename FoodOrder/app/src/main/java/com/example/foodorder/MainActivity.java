34f53c7a63060e46eaf4faa507ffd0aeeb4b75e8
HaMinhKhoi29092000 <haminhkhoi.29092000.14@gmail.com>
2021-04-01 13:31:22 +0700

    add intent

--- FoodOrder/app/src/main/java/com/example/foodorder/MainActivity.java
+++ FoodOrder/app/src/main/java/com/example/foodorder/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.foodorder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
-
+    RelativeLayout item;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu);
+        item = findViewById(R.id.taco);
+        item.setOnClickListener(clickItemListener);
+    }
+
+    private final View.OnClickListener clickItemListener = new View.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+           openOrder();
+        }
+    };
+
+    public void openOrder()
+    {
+        Intent i = new Intent(this, OrderActivity.class);
+        startActivity(i);
     }
 }
---
+++ FoodOrder/app/src/main/java/com/example/foodorder/OrderActivity.java
@@ -0,0 +1,16 @@
+package com.example.foodorder;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OrderActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.order);
+    }
+}
