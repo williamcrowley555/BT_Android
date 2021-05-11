ef790abd15856c914bab4aad198dfc8235820634
William <buiquanghuy.10122000.10@gmail.com>
2021-05-11 22:10:26 +0700

    Music Player/ feat: view song list + play music

--- MusicPlayer/app/src/main/java/com/example/musicplayer/MainActivity.java
+++ MusicPlayer/app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -2,13 +2,133 @@ package com.example.musicplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.io.File;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    String[] items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_player);
+        setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.listViewSong);
+
+        runtimePermission();
+    }
+
+    public void runtimePermission() {
+        Dexter.withContext(this).withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                        displaySongs();
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                        permissionToken.continuePermissionRequest();
+                    }
+                }).check();
+    }
+
+    public ArrayList<File> findSong(File file) {
+        ArrayList arrayList = new ArrayList();
+        File [] files = file.listFiles();
+
+        if(files != null) {
+            for (File singlefile : files) {
+                if (singlefile.isDirectory() && !singlefile.isHidden()) {
+                    arrayList.addAll(findSong(singlefile));
+                } else {
+                    if (singlefile.getName().endsWith(".wav")) {
+                        arrayList.add(singlefile);
+                    } else if (singlefile.getName().endsWith(".mp3")) {
+                        arrayList.add(singlefile);
+                    }
+                }
+
+            }
+        }
+        return arrayList;
+    }
+
+    public void displaySongs() {
+        final ArrayList<File> mySongs = findSong(Environment.getExternalStorageDirectory());
+        items = new String[mySongs.size()];
+
+        for (int i = 0; i < mySongs.size(); ++i) {
+            items[i] = mySongs.get(i).getName().toString().replace(".mp3", "".replace(".wav", ""));
+        }
+
+        CustomAdapter customAdapter = new CustomAdapter();
+        listView.setAdapter(customAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String songName = (String) listView.getItemAtPosition(position);
+                Intent intent = new Intent(getApplicationContext(), PlayerActivity.class);
+                intent.putExtra("songs", mySongs)
+                        .putExtra("songName", songName)
+                        .putExtra("position", position);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private class CustomAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return items.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View myView = getLayoutInflater().inflate(R.layout.list_item, null);
+            TextView textSong = myView.findViewById(R.id.txtSongName);
+            textSong.setSelected(true);
+            textSong.setText(items[position]);
+
+            return myView;
+        }
     }
 }
--- MusicPlayer/app/src/main/java/com/example/musicplayer/PlayerActivity.java
+++ MusicPlayer/app/src/main/java/com/example/musicplayer/PlayerActivity.java
@@ -2,13 +2,88 @@ package com.example.musicplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import com.gauravk.audiovisualizer.visualizer.BarVisualizer;
+
+import java.io.File;
+import java.util.ArrayList;
 
 public class PlayerActivity extends AppCompatActivity {
+
+    Button btnPlay, btnNext, btnPrev, btnff, btnfr;
+    TextView txtSongName, txtSongStart, txtSongStop;
+    SeekBar seekMusic;
+    BarVisualizer visualizer;
+
+    String sName;
+    public static final String EXTRA_NAME = "song_name";
+    static MediaPlayer mediaPlayer;
+    int position;
+    ArrayList<File> mySongs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
+
+        mapView();
+
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+        }
+
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+
+        mySongs = (ArrayList) bundle.getParcelableArrayList("songs");
+        String songName = intent.getStringExtra("songName");
+        position = bundle.getInt("position", 0);
+
+        txtSongName.setSelected(true);
+        Uri uri = Uri.parse(mySongs.get(position).toString());
+        sName = mySongs.get(position).getName();
+        txtSongName.setText(sName);
+
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), uri);
+        mediaPlayer.start();
+
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mediaPlayer.isPlaying()) {
+                    btnPlay.setBackgroundResource(R.drawable.ic_play);
+                    mediaPlayer.pause();
+                }
+                else {
+                    btnPlay.setBackgroundResource(R.drawable.ic_pause);
+                    mediaPlayer.start();
+                }
+            }
+        });
+    }
+
+    private void mapView() {
+        btnPrev = (Button) findViewById(R.id.btnPrev);
+        btnNext = (Button) findViewById(R.id.btnNext);
+        btnPlay = (Button) findViewById(R.id.btnPlay);
+        btnff = (Button) findViewById(R.id.btnFastForward);
+        btnfr = (Button) findViewById(R.id.btnFastRewind);
+
+        txtSongName = (TextView) findViewById(R.id.txtSongName);
+        txtSongStart = (TextView) findViewById(R.id.txtMusicStart);
+        txtSongStop = (TextView) findViewById(R.id.txtMusicStop);
+
+        seekMusic = (SeekBar) findViewById(R.id.seekbar);
+
+        visualizer = (BarVisualizer) findViewById(R.id.blast);
     }
 }
